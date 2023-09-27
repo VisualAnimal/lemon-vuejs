4f5626ad35089dda48b2352320c962bdd5f20f42
VisualAnimal <visualanimal@icloud.com>
2023-09-27 18:24:00 +0800

    新增编辑描述功能

--- src/views/EditPage.vue
+++ src/views/EditPage.vue
@@ -4,8 +4,8 @@
     <div class="container">
         <div class="left">
             <van-sticky>
-                <van-sidebar v-model="active" v-if="responseData.value">
-                    <van-sidebar-item v-for="item in responseData.value" :title="item.name" :key="item._id" />
+                <van-sidebar v-model="active" v-if="categories.value">
+                    <van-sidebar-item v-for="item in categories.value" :title="item.name" :key="item._id" />
                 </van-sidebar>
             </van-sticky>
         </div>
@@ -13,10 +13,28 @@
             <!-- 使用v-for迭代渲染每个产品的卡片 -->
             <van-card v-for="product in products.value" :price="product.price" :desc="product.description"
                 :title="product.name" :thumb="'http://45.207.55.130:3000/xiaoman/' + product.imageUrl">
+                <template v-if="product.description" #desc>
+                    <div class="desc" @click="() => showEditDescriptionPopup(product)">{{ product.description }}</div>
+                    <van-popup v-model:show="product.showEditDescription" position="top" :key="product._id">
+                        <van-field v-model="editDescription" label="描述" placeholder="输入产品描述"
+                            @keyup.enter="() => saveDescription(product)" />
+                        <van-button @click="() => saveDescription(product)">保存描述</van-button>
+                    </van-popup>
+                </template>
+                <template else #desc>
+                    <div class="desc" @click="() => showEditDescriptionPopup(product)">添加描述内容</div>
+                    <van-popup v-model:show="product.showEditDescription" position="top" :key="product._id">
+                        <van-field v-model="editDescription" label="描述" placeholder="输入产品描述"
+                            @keyup.enter="() => saveDescription(product)" />
+                        <van-button @click="() => saveDescription(product)">保存描述</van-button>
+                    </van-popup>
+                </template>
+
 
                 <template #tags>
                     <template v-for="(tag, index) in product.tags">
-                        <van-tag class="tag" plain closeable @close="() => deleteTag(product, index)" type="primary">{{ tag
+                        <van-tag class="tag" plain closeable @close="() => deleteTag(product, index)" type="primary">{{
+                            tag
                         }}</van-tag>
                     </template>
                     <van-tag class="tag" type="success" @click="() => showPopup(product)">添加标签</van-tag>
@@ -37,29 +55,32 @@ import { onMounted, ref } from 'vue';
 import { reactive } from 'vue';
 import Header from '@/components/Header.vue'
 
-// const API_URL = 'http://127.0.0.1:3000';
 const active = ref(0)
 const API_URL = 'http://45.207.55.130:3000';
-
-const responseData = reactive([]);
-
+const categories = reactive([]);
 const products = reactive([]);
 
+// 获取数据
 async function fetchData() {
     try {
-        const response = await axios.get(`${API_URL}/categories`);
-        responseData.value = response.data;
-        products.value = response.data.map(product => ({ ...product, showPopup: false }));
+        // 获取菜单数据
+        const categories_response = await axios.get(`${API_URL}/categories`);
+        categories.value = categories_response.data
 
         // 初始化每个类别的编辑状态和更新后的名称
-        responseData.value.forEach(category => {
+        categories.value.forEach(category => {
             category.editing = false;
             category.updatedName = category.name;
         });
 
-        // 获取产品数据，假设产品数据从另一个 API 端点获取
-        const productResponse = await axios.get(`${API_URL}/products`);
-        products.value = productResponse.data;
+        // 获取产品数据
+        const product_response = await axios.get(`${API_URL}/products`);
+        // 注意：你还需要在产品数据映射时为每个产品对象添加一个`showEditDescription`属性，并设置其值为false。
+        products.value = product_response.data.map(product => ({
+            ...product,
+            showPopup: false,
+            showEditDescription: false
+        }));
     } catch (error) {
         console.error('请求错误:', error);
     }
@@ -70,19 +91,32 @@ onMounted(() => {
     fetchData();
 });
 
+// 修改描述内容
+const editDescription = ref('');   // 存储用户输入的新描述
+const showEditDescriptionPopup = (product) => {
+    product.showEditDescription = true;
+    editDescription.value = product.description || '';  // 初始化为当前描述或为空
+}
+async function saveDescription(product) {
+    product.description = editDescription.value;
+
+    try {
+        await axios.put(`${API_URL}/products/${product._id}`, { description: product.description });
+
+    } catch (error) {
+        console.error('更新描述失败:', error);
+    }
+    editDescription.value = '';           // 清空输入框
+    product.showEditDescription = false;
+}
+
 // 添加标签
-const show = ref(false);   // 控制弹出对话框的显示/隐藏
 const showPopup = (product) => {
     product.showPopup = true;
 }
 const newTag = ref('');               // 存储用户输入的新标签
 
 async function addTag(product) {
-    console.log("All Products:", product);
-
-    console.log("New Tag:", newTag.value.trim());
-    console.log("Original Tags:", product.tags);
-
     if (newTag.value.trim() !== '') {
         product.tags.push(newTag.value.trim());
         try {
@@ -95,7 +129,7 @@ async function addTag(product) {
             // alert('更新标签失败，请稍后再试');
         }
         newTag.value = '';           // 清空输入框
-        show.value = false; // 关闭对话框
+        product.showPopup = false;
     }
 }
 
@@ -143,6 +177,14 @@ const deleteTag = async (product, index) => {
 
 .tag {
     margin-right: 3px;
+}
+
+.desc {
+    color: var(--van-text-color-3);
+    word-wrap: break-word;
+    /* Legacy browsers */
+    overflow-wrap: break-word;
+    /* Modern browsers */
 }
 </style>
   
